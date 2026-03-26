84274654f821de3bd1659f7702c5c4d1d6cca449
bettercallnehi <minhtriphanthe@gmail.com>
2026-03-26 09:10:38 +0700

    Update inverse.py

--- part1/inverse.py
+++ part1/inverse.py
@@ -1,1 +1,7 @@
-
+def Inverse(A): # Hàm tính A^(-1) bằng phương pháp Gauss-Jordan
+   n = A.rows # Lấy số hàng của A
+   I_n = [[0 for _ in range(n)] for _ in range(n)] # Tạo ma trận đơn vị I_n
+   for i in range(A.rows):
+      I_n[i][i] = 1
+   gaussian_eliminated_2(new_matrix, I_n) # Thực hiện RREF trên [new_matrix | I_n]
+   return I_n
